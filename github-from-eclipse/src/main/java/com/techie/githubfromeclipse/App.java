24cf09dc12d24b0749e868935a550fc630ab5651
admin <admin@DESKTOP-3LNR19P>
2021-06-23 16:47:34 +0530

    second commit with changes

--- github-from-eclipse/src/main/java/com/techie/githubfromeclipse/App.java
+++ github-from-eclipse/src/main/java/com/techie/githubfromeclipse/App.java
@@ -3,7 +3,7 @@ package com.techie.githubfromeclipse;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world First change from java techie");
 
 	}
 
