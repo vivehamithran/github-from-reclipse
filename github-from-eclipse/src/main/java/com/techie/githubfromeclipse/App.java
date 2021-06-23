397dde65e2aa0c6c793eb6ac2abf34730d46fc90
vivehamithran <75566679+vivehamithran@users.noreply.github.com>
2021-06-23 17:19:41 +0530

    johns update

--- github-from-eclipse/src/main/java/com/techie/githubfromeclipse/App.java
+++ github-from-eclipse/src/main/java/com/techie/githubfromeclipse/App.java
@@ -4,6 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world First change from java techie");
+		System.out.println("Hello world First change from java techie");
 
 	}
 
